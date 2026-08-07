e11ed75660fc9032646d11bfb8e57f405b22fba8
aadi1817 <aadirastogi1817@gmail.com>
2026-08-07 20:27:17 +0530

    Time: 4 ms (81.30%) | Memory: 10.3 MB (97.04%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,19 +1,25 @@
 class Solution {
 public:
-    int helper(int i,int j,string s1,string s2,vector<vector<int>>& dp){
-        if(i<0) return j+1;
-        if(j<0) return i+1;
-        if(s1[i]==s2[j]) return helper(i-1,j-1,s1,s2,dp);
-        if(dp[i][j]!=-1) return dp[i][j];
-        int ins=1+helper(i,j-1,s1,s2,dp);
-        int del=1+helper(i-1,j,s1,s2,dp);
-        int rep=1+helper(i-1,j-1,s1,s2,dp);
-        return dp[i][j]=min(rep,min(ins,del));
-    }
     int minDistance(string s1, string s2) {
         int n=s1.length();
         int m=s2.length();
-        vector<vector<int>> dp(n,vector<int>(m,-1));
-        return helper(n-1,m-1,s1,s2,dp);
+        vector<int> dp(m+1,0),curr(m+1,0);
+        for(int j=0;j<m+1;j++){
+            dp[j]=j;
+        }
+        for(int i=1;i<n+1;i++){
+            curr[0]=i;
+            for(int j=1;j<m+1;j++){
+                if(s1[i-1]==s2[j-1]) curr[j]=dp[j-1];
+                else{
+                    int ins=1+curr[j-1];
+                    int del=1+dp[j];
+                    int rep=1+dp[j-1];
+                    curr[j]=min(rep,min(ins,del));
+                }
+            }
+            dp=curr;
+        }
+        return dp[m];
     }
 };
