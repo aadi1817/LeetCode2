6fcd5ed4184207c39e842d09d5fcfcd25b320b29
aadi1817 <aadirastogi1817@gmail.com>
2026-08-09 17:28:12 +0530

    Time: 6 ms (9.03%) | Memory: 16.1 MB (64.90%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
-    int helper(int i,int b,vector<int>& prices,vector<vector<int>>& dp){
-        if(i>=prices.size()) return 0;
-        if(dp[i][b]!=-1) return dp[i][b];
-        if(b==1){
-            return dp[i][b]=max(-prices[i]+helper(i+1,0,prices,dp),helper(i+1,1,prices,dp));
-        }
-        return dp[i][b]=max(prices[i]+helper(i+2,1,prices,dp),helper(i+1,0,prices,dp));
-    }
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
-        vector<vector<int>> dp(n,vector<int>(2,-1));
-        return helper(0,1,prices,dp);
+        vector<vector<int>> dp(n+2,vector<int>(2,0));
+        for(int i=0;i<=1;i++){
+            dp[n][i]=0;
+            dp[n+1][i]=0;
+        }
+        for(int i=n-1;i>=0;i--){
+            for(int b=0;b<=1;b++){
+                if(b==1) dp[i][b]=max(-prices[i]+dp[i+1][0],dp[i+1][1]);
+                else dp[i][b]=max(prices[i]+dp[i+2][1],dp[i+1][0]);
+            }
+        }
+        return dp[0][1];
     }
 };
