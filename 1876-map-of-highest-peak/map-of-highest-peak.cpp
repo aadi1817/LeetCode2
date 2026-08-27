50b006b35ffdead4bc10bd728f8c3b5ee6ad7664
aadi1817 <aadirastogi1817@gmail.com>
2026-08-27 16:37:47 +0530

    Time: 94 ms (25.77%) | Memory: 128.3 MB (10.02%) - LeetSync

---
+++ 1876-map-of-highest-peak/map-of-highest-peak.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<vector<int>> highestPeak(vector<vector<int>>& mat) {
+        int n=mat.size();
+        int m=mat[0].size();
+        vector<vector<int>> visited(n,vector<int>(m,0)), distance(n,vector<int>(m,0));
+        queue<pair<pair<int,int>,int>> q;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(mat[i][j]==1){
+                    q.push({{i,j},0});
+                    visited[i][j]=1;
+                }
+            }
+        }
+        vector<int> drow={-1,0,1,0};
+        vector<int> dcol={0,1,0,-1};
+        while(!q.empty()){
+            int row=q.front().first.first;
+            int col=q.front().first.second;
+            int dis=q.front().second;
+            q.pop();
+            distance[row][col]=dis;
+            for(int i=0;i<4;i++){
+                int nrow=row+drow[i];
+                int ncol=col+dcol[i];
+                if(nrow>=0 && ncol>=0 && nrow<n && ncol<m && visited[nrow][ncol]==0){
+                    visited[nrow][ncol]=1;
+                    q.push({{nrow,ncol},dis+1});
+                }
+            }
+        }
+        return distance;
+    }
+};
